f9399cc1492ccd2e0e16b2dafe29760ef02bac85
Dosu333 <vladamir1865@gmail.com>
2021-10-03 18:16:38 +0100

    Changed allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=!%l7&xz06x3b6uvdg-qf%st)py%)dv(74(@4%5v5(23*u67)#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'fixnigeria.herokuapp.com']
 
 
 # Application definition
