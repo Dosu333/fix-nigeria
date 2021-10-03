cff64a414d5f2658b27816671a653ab49eb3dc67
Dosu333 <vladamir1865@gmail.com>
2021-10-03 18:25:21 +0100

    Changed allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -136,15 +136,16 @@ prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_TMP = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/static/'
+STATIC_TMP = '/static/'
+STATIC_URL = '/static/'
 os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
-STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static")
+# ]
 
 
 # Default primary key field type
